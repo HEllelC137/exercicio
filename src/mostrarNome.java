91e6971853e3029be9b5979725a7eee6ba3d2774
Brayan <brayan.carlos.moraes@escola.pr.gov.br>
2024-09-30 11:28:09 -0300

    iniciando os arquivos

---
+++ src/mostrarNome.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class mostrarNome {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        SQLOutput informeSeuNome;
+
+
+    }
+}
